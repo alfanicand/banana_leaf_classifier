cc13dd17a9a3af56f163d1837ab7002fc65076fb
alfanicand <167304329+alfanicand@users.noreply.github.com>
2026-02-09 13:35:26 +0700

    Refactor app.py for model comparison and UI

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ CLASS_NAMES = ['cordana', 'healthy', 'pestalotiopsis', 'sigatoka']
 
 CONF_THRESHOLDS = {
     "Fixed Feature": 0.50,
-    "FT10": 0.55,
+    "FT10": 0.50,
     "FT20": 0.50,
-    "FT30": 0.57
+    "FT30": 0.50
 }
 
 
@@ -140,6 +140,7 @@ if uploaded_file is not None:
             st.bar_chart(
                 {CLASS_NAMES[i]: float(pred_ef[i]) for i in range(len(CLASS_NAMES))}
             )
+
 
 
 
